5ec80211d4d8032140b0b6dcb9d2d0bf550a644d
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-07-03 16:31:57 +0530

    xml updated

--- pesistance-service/src/main/java/lk/ijse/pesistanceservice/repository/CategoryRepository.java
+++ pesistance-service/src/main/java/lk/ijse/pesistanceservice/repository/CategoryRepository.java
@@ -1,4 +1,8 @@
 package lk.ijse.pesistanceservice.repository;
 
-public class CategoryRepository {
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public class CategoryRepository extends JpaRepository<CategoryRepository, Long>{
 }
