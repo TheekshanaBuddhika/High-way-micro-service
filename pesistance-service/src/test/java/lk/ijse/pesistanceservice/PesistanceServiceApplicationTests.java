b31205d5ae2fc8a82d1fd140d21dfd0a5c811bd3
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-07-03 15:57:25 +0530

    persistance updated

---
+++ pesistance-service/src/test/java/lk/ijse/pesistanceservice/PesistanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.pesistanceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesistanceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
