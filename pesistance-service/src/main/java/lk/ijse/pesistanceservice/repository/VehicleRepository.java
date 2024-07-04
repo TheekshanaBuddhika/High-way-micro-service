fcad60c93f08bf23475d805b890b000e3fdd754a
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-07-04 20:39:12 +0530

    updated

--- pesistance-service/src/main/java/lk/ijse/pesistanceservice/repository/VehicleRepository.java
+++
@@ -1,9 +0,0 @@
-package lk.ijse.pesistanceservice.repository;
-
-import lk.ijse.pesistanceservice.entity.VehicleEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VehicleRepository extends JpaRepository<VehicleEntity, Long> {
-}
